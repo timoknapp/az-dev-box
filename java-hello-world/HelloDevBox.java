a5287ce698f9112c2050db1e68110da80f318adb
timoknapp <mail@timoknapp.com>
2023-10-09 22:00:26 +0200

    chore: update hello world

---
+++ java-hello-world/HelloDevBox.java
@@ -0,0 +1,6 @@
+// Run: javac Hello.java && java Hello
+public class HelloDevBox {
+    public static void main(String[] args) {
+        System.out.println("Hello, world from Microsoft Dev Box!");
+    }
+}
